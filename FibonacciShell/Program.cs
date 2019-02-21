f5f10fb1d7bfe5b95c1c0b7c4f6740c605d597d1
Jukka-Pekka Stranden <jukka-pekka.stranden@protacon.com>
2019-02-21 13:23:07 +0200

    Add project files.

---
+++ FibonacciShell/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FibonacciShell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
